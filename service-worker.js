6d9ff2d1612dccc28ef3ca13fd34b173be2aa29e
Julian Wildmann <julian.wildmann@freenet.de>
2019-05-29 08:03:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/online-shop/precache-manifest.3c9b9d46382aa850ab225452216ad51f.js"
+  "/online-shop/precache-manifest.0c448e5ce3c98b1303de8d40a0318ef7.js"
 );
 
 self.addEventListener('message', (event) => {
