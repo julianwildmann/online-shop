ef20d33d7c4767cc5f0b0980230a3215bd6efd89
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-03 05:59:07 +0200

    clean repo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from "./components/layout/Header";
+import Header from "./components/Header";
 import Gallery from "./components/Gallery";
 
 
 class App extends Component {
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: "Take out the trash",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Dinner with wife",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Meeting with boss",
-        completed: false
-      },
-    ]
-  }
   render() {
     return (
       <div className="App">
