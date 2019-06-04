a6c775497946c83d3473de2e0583d5d75989b9f0
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-04 12:09:19 +0200

    chair update and product photos

---
+++ build/precache-manifest.d03f1742a8ade79530b2b0a98b22a80c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b7c857db46483816f17c6251a641a7",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "9cf802a2020d848c3082",
+    "url": "/online-shop/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "847d06b3edb696562f6c",
+    "url": "/online-shop/static/css/main.27acd5e3.chunk.css"
+  },
+  {
+    "revision": "9cf802a2020d848c3082",
+    "url": "/online-shop/static/js/2.fbcfb491.chunk.js"
+  },
+  {
+    "revision": "847d06b3edb696562f6c",
+    "url": "/online-shop/static/js/main.6fb42acb.chunk.js"
+  },
+  {
+    "revision": "de22ddf6103542cfd170",
+    "url": "/online-shop/static/js/runtime~main.b83c20ff.js"
+  },
+  {
+    "revision": "5e487c2325ba9003e613096f6168b7f7",
+    "url": "/online-shop/static/media/product-image-11.5e487c23.jpg"
+  },
+  {
+    "revision": "677834bd4bec1a03e2010ffc5ba26dd6",
+    "url": "/online-shop/static/media/product-image-21.677834bd.jpg"
+  },
+  {
+    "revision": "aa3514d02a33cb5f391cfd39b0cae598",
+    "url": "/online-shop/static/media/product-image-31.aa3514d0.jpg"
+  },
+  {
+    "revision": "80206bd6c2b0ea2428b57a669cd35e9b",
+    "url": "/online-shop/static/media/product-image-41.80206bd6.jpg"
+  },
+  {
+    "revision": "8995358e3829062b8c549be04e9896ab",
+    "url": "/online-shop/static/media/product-image-51.8995358e.jpg"
+  }
+]);
