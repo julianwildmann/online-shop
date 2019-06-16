4b1834fc5abf786fdd1c28c60ccb7d56b0d2706a
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-16 20:23:40 +0200

    model viewer

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "@google/model-viewer"
+import "./ModelViewer.css"
 
 class ModelViewer extends React.Component { 
     render() {
         return (
-            <div className="ModelViewer">
-                 <model-viewer src="media/3D-files/BlenderFinal.gltf" ar camera-controls alt="DSW Chair" camera-controls background-color="#FFFFFF" ios-src="media/3D-Files/DSWMaterialFinal.usdz" position="center" background-color="#455A64"></model-viewer>
+            <div id="ModelViewer">
+                 <model-viewer src="media/3D-files/BlenderFinal.gltf" ar ios-src="media/3D-Files/DSWMaterialFinal.usdz" controls poster="DSW_Poster.png" alt="DSW Chair" background-color="#FFFFFF"  position="center" background-color="#455A64"></model-viewer>
           </div>
         )
     }
