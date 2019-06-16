d71a74ef2f9caa9de6aacb5c4693577e488865dc
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-16 20:24:24 +0200

    Updates

---
+++ precache-manifest.e8dbf5a3ec119301b02b3e7554fd267d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8f5706b95b4c2e8a77f293fe11898e",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "bee5576f6286b82299d3",
+    "url": "/online-shop/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "708b35e658e22ef92436",
+    "url": "/online-shop/static/css/main.1ed3569f.chunk.css"
+  },
+  {
+    "revision": "bee5576f6286b82299d3",
+    "url": "/online-shop/static/js/2.708210d5.chunk.js"
+  },
+  {
+    "revision": "708b35e658e22ef92436",
+    "url": "/online-shop/static/js/main.2cae7025.chunk.js"
+  },
+  {
+    "revision": "de22ddf6103542cfd170",
+    "url": "/online-shop/static/js/runtime~main.b83c20ff.js"
+  },
+  {
+    "revision": "f67bd403a905b32c67d699c45ead3d1f",
+    "url": "/online-shop/static/media/product-image-11.f67bd403.jpg"
+  },
+  {
+    "revision": "18caa11b6bc4ac42d4c8321c3ea5295c",
+    "url": "/online-shop/static/media/product-image-21.18caa11b.jpg"
+  },
+  {
+    "revision": "a1e40e13e9e19a6e4b4c7aba3f29e67f",
+    "url": "/online-shop/static/media/product-image-31.a1e40e13.jpg"
+  },
+  {
+    "revision": "c8a832bcfcb7e37004c53c21c62c697a",
+    "url": "/online-shop/static/media/product-image-41.c8a832bc.jpg"
+  },
+  {
+    "revision": "176906a4e4ee41a8aaae26edc7c2c823",
+    "url": "/online-shop/static/media/product-image-51.176906a4.jpg"
+  }
+]);
