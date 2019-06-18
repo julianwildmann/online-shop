d3f7f13252885ed7ce1b44945268f6b035160774
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-18 09:53:25 +0200

    model viewer

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -5,8 +5,8 @@ import "./ModelViewer.css"
 class ModelViewer extends React.Component { 
     render() {
         return (
-            <div id="ModelViewer">
-                 <model-viewer ar src="media/3D-files/DWS.gltf" ios-src="media/3D-files/DSWMaterialFinal.usdz" camera-controls poster="DSW_Poster.png" alt="DSW Chair" background-color="#555555" auto-rotate align-model="center"></model-viewer>
+            <div id="ModelViewer" width="700px" height="400px">
+                 <model-viewer width="750px" ar src="media/3D-files/DWS-images.gltf" ios-src="media/3D-files/DSWMaterialFinal.usdz" camera-controls poster="media/3D-files/DSW_Poster.png" alt="DSW Chair" background-color="#555555" auto-rotate ></model-viewer>
           </div>
         )
     }
