ae64fafee26ae2851792cb5725c3bbde42e9e573
Julian Wildmann <julian.wildmann@freenet.de>
2019-06-23 20:47:37 +0200

    osVersion

---
+++ build/precache-manifest.993ffd928aa12a6cc013ccb777aac7f9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb04ca8affd7ffe0935f8f075f48b68f",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "c7fa61ce77508300d443",
+    "url": "/online-shop/static/css/2.a881067f.chunk.css"
+  },
+  {
+    "revision": "8d341f4300ac00a2a404",
+    "url": "/online-shop/static/css/main.cf77336f.chunk.css"
+  },
+  {
+    "revision": "c7fa61ce77508300d443",
+    "url": "/online-shop/static/js/2.c2037048.chunk.js"
+  },
+  {
+    "revision": "8d341f4300ac00a2a404",
+    "url": "/online-shop/static/js/main.c2c756cf.chunk.js"
+  },
+  {
+    "revision": "de22ddf6103542cfd170",
+    "url": "/online-shop/static/js/runtime~main.b83c20ff.js"
+  },
+  {
+    "revision": "f67bd403a905b32c67d699c45ead3d1f",
+    "url": "/online-shop/static/media/product-image-11.f67bd403.jpg"
+  },
+  {
+    "revision": "18caa11b6bc4ac42d4c8321c3ea5295c",
+    "url": "/online-shop/static/media/product-image-21.18caa11b.jpg"
+  },
+  {
+    "revision": "a1e40e13e9e19a6e4b4c7aba3f29e67f",
+    "url": "/online-shop/static/media/product-image-31.a1e40e13.jpg"
+  },
+  {
+    "revision": "c8a832bcfcb7e37004c53c21c62c697a",
+    "url": "/online-shop/static/media/product-image-41.c8a832bc.jpg"
+  },
+  {
+    "revision": "176906a4e4ee41a8aaae26edc7c2c823",
+    "url": "/online-shop/static/media/product-image-51.176906a4.jpg"
+  }
+]);
--- src/components/ModelViewerAR.js
+++ src/components/ModelViewerAR.js
@@ -9,7 +9,7 @@ class ModelViewerAR extends React.Component {
             <div id="ModelViewerContainer">
                 <div>
                     <model-viewer id="ModelViewerAR"
-                        ar src="media/3D-files/DSW.gltf" ios-src="media/3D-files/DSWMaterialFinal.usdz" poster="media/3D-files/DSW_Poster.png" alt="DSW Chair" background-color="#FFFFFF" camera-controls>
+                        ar src="media/3D-files/DSW.gltf" ios-src="media/3D-files/BlenderFinal1.usdz" poster="media/3D-files/DSW_Poster.png" alt="DSW Chair" background-color="#FFFFFF" camera-controls>
                     </model-viewer>
                 </div>
                 <p id="instruction">Tippe auf den Stuhl um ihn in deine Umgebung zu platzieren</p>
